3585e8635d4b2a10cc46329d058c6e02958d6874
Paulo Dias <paulo.dias@outlook.com>
2015-08-03 11:01:17 -0300

    Novo comentário

--- Rekool/www/js/rekool.js
+++ Rekool/www/js/rekool.js
@@ -1,39 +1,39 @@
 var rekoolapp = angular.module('rekoolapp', ['ionic']);
 
 
-rekoolapp.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+rekoolapp.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 });
 
-rekoolapp.config(function($stateProvider, $urlRouterProvider) {
+rekoolapp.config(function ($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('login', {
-      url: "/login",
-      templateUrl: "/pages/login.html"
-    })
-    .state('cadastro', {
-      url: "/cadastro",
-      templateUrl: "/pages/cadastro.html"
+    $stateProvider
+      .state('login', {
+          url: "/login",
+          templateUrl: "pages/login.html"
+      })
+      .state('cadastro', {
+          url: "/cadastro",
+          templateUrl: "pages/cadastro.html"
       });
 
-   $urlRouterProvider.otherwise("/login");
+    $urlRouterProvider.otherwise("/login");
 
 })
 
-rekoolapp.controller('loginController', function($scope) {
-  console.log("teste de Login");
+rekoolapp.controller('loginController', function ($scope) {
+   // console.log("teste de Login");
 });
 
-rekoolapp.controller('cadastroController', function($scope) {
-  console.log("teste de cadastro");
+rekoolapp.controller('cadastroController', function ($scope) {
+    //console.log("teste de cadastro");
 });
